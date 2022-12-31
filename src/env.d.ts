717adcd1e05c634f24934f9f0752a9aa75ee31a1
penrodlol <christian@penrod.contact>
2022-12-31 16:55:54 -0500

    🔍 Some twitter og image stuff

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,9 +4,9 @@ interface ImportMetaEnv {
   readonly EMAIL: string;
   readonly JOB: string;
   readonly COLLEGE: string;
+  readonly USERNAME: string;
   readonly GITHUB_PROFILE: string;
   readonly GITHUB_TOKEN: string;
-  readonly GITHUB_USERNAME: string;
 }
 
 interface ImportMeta {
