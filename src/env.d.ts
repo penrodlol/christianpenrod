efebe020a1e17a2450e82c9ed18aa97201c9f99b
penrodlol <christian@penrod.contact>
2022-12-30 22:21:21 -0500

    ⚰️ No longer using supabase

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,8 +7,6 @@ interface ImportMetaEnv {
   readonly GITHUB_PROFILE: string;
   readonly GITHUB_TOKEN: string;
   readonly GITHUB_USERNAME: string;
-  readonly PUBLIC_SUPABASE_URL: string;
-  readonly PUBLIC_SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
