8022024bed57b75ec978691b55b37634004bf4d4
penrodlol <christian@penrod.contact>
2023-02-01 21:14:02 -0500

    ✨ replace prisma in favor of netlify's planetscale integration

--- netlify/functions/view-post.ts
+++ netlify/functions/view-post.ts
@@ -1,27 +1,26 @@
 import type { Handler } from '@netlify/functions';
-import { PrismaClient } from '@prisma/client';
+import { withPlanetscale } from '@netlify/planetscale';
 import { z } from 'zod';
 
-const db = new PrismaClient();
-
-const handler: Handler = async (event) => {
+export const handler: Handler = withPlanetscale(async (event, ctx) => {
   if (event.httpMethod !== 'PUT') return { statusCode: 405 };
   if (!event.body) return { statusCode: 400 };
 
-  const payload = z
-    .object({ slug: z.string() })
-    .safeParse(JSON.parse(event.body));
+  const body = JSON.parse(event.body);
+  const request = z.object({ slug: z.string() }).safeParse(body);
 
-  if (!payload.success) return { statusCode: 400 };
+  if (!request.success) return { statusCode: 400 };
 
-  const { views } = await db.postView.upsert({
-    update: { views: { increment: 1 } },
-    create: { post: payload.data.slug },
-    where: { post: payload.data.slug },
-    select: { views: true },
+  const slug = request.data.slug;
+  const db = ctx.planetscale.connection;
+  const rs = await db.transaction(async (t) => {
+    await t.execute('UPDATE PostView SET views=views+1 WHERE post=?', [slug]);
+    return t.execute('SELECT views FROM PostView WHERE post=?', [slug]);
   });
 
-  return { statusCode: 200, body: String(views) };
-};
+  const response = z.object({ views: z.number() }).safeParse(rs.rows[0]);
 
-export { handler };
+  if (!response.success) return { statusCode: 500 };
+
+  return { statusCode: 200, body: String(response.data.views) };
+});
