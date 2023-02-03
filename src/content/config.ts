5664563014ab06132b1bbb90a03440059407ea2f
penrodlol <christian@penrod.contact>
2023-02-02 19:18:32 -0500

    ✨ astro 2

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,20 @@
 import { defineCollection, z } from 'astro:content';
 
 const posts = defineCollection({
-  schema: {
+  schema: z.object({
     title: z.string(),
     description: z.string(),
     published: z.date(),
     repo: z.string().optional(),
-  },
+  }),
 });
 
 const roles = defineCollection({
-  schema: {
+  schema: z.object({
     company: z.string(),
     start: z.date(),
     end: z.date().optional(),
-  },
+  }),
 });
 
 export const collections = { posts, roles };
