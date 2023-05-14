eaf0301505bf9db44c025cab577b14f1bc11bb70
Christian <christian@penrod.contact>
2023-05-14 17:47:31 -0400

    feature/astro-assets
    * ✨ astro assets
    * ✨ Better og image alt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 import { toString } from 'mdast-util-to-string';
 import readingtime from 'reading-time';
 
 export default defineConfig({
   site: 'https://christianpenrod.com',
+  image: { service: sharpImageService() },
+  experimental: { assets: true },
   markdown: {
     shikiConfig: { theme: 'css-variables' },
     rehypePlugins: [
@@ -24,7 +25,6 @@ export default defineConfig({
     tailwind(),
     mdx(),
     preact(),
-    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
     sitemap({ changefreq: 'daily', lastmod: new Date() }),
     robotsTxt({
       host: true,
