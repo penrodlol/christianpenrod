635a5a232da5167ff32cadb4dfbcc923aae168b2
penrodlol <christian@penrod.contact>
2023-05-14 21:16:45 -0400

    ✨ daily build cron joib

---
+++ api/daily-build.ts
@@ -0,0 +1,11 @@
+export const config = { runtime: 'edge' };
+import { z } from 'zod';
+
+export default async function handler(): Promise<Response> {
+  const token = z.string().safeParse(process.env.VERCEL_DEPLOY_WEBHOOK_TOKEN);
+  if (!token.success) return new Response('Unauthorized', { status: 401 });
+
+  return fetch(`https://api.vercel.com/v1/integrations/deploy/${token.data}`)
+    .then((res) => new Response(JSON.stringify(res.json()), { status: 200 }))
+    .catch((err) => new Response(err.message, { status: 500 }));
+}
