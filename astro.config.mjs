42d72cd4499ecb7afec30d0c13511a39a96ce60b
penrodlol <christian@penrod.contact>
2023-05-31 17:47:01 -0400

    ✨ astro vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 import robotsTxt from 'astro-robots-txt';
 import { defineConfig, sharpImageService } from 'astro/config';
 import { toString } from 'mdast-util-to-string';
@@ -12,6 +13,7 @@ export default defineConfig({
   site: 'https://christianpenrod.com',
   image: { service: sharpImageService() },
   experimental: { assets: true },
+  adapters: [vercel({ analytics: true })],
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [
