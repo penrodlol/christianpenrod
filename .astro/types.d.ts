80217499566085c5dddf2ae5af52dd104edc502e
penrodlol <christian@penrod.contact>
2023-08-30 20:56:10 -0400

    🏗️ Prepare posts to have cover images

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -207,8 +207,8 @@ declare module 'astro:content' {
   collection: "posts";
   data: InferEntrySchema<"posts">
 } & { render(): Render[".mdx"] };
-"summarize-blog-posts-with-typescript-and-openais-gpt.mdx": {
-	id: "summarize-blog-posts-with-typescript-and-openais-gpt.mdx";
+"summarize-blog-posts-with-typescript-and-openais-gpt/index.mdx": {
+	id: "summarize-blog-posts-with-typescript-and-openais-gpt/index.mdx";
   slug: "summarize-blog-posts-with-typescript-and-openais-gpt";
   body: string;
   collection: "posts";
