58fa4329f350a90c42b4bfe00e8e39173f085f0d
penrodlol <christian@penrod.contact>
2023-09-04 16:30:05 -0400

    ✨ rss

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss';
+import type { APIRoute } from 'astro';
+import { getCollection } from 'astro:content';
+
+export const get: APIRoute = async () => {
+  const posts = await getCollection('posts');
+
+  return rss({
+    title: 'Blog | Christian Penrod',
+    description: 'A blog tailoring to Web Development.',
+    site: import.meta.env.SITE,
+    items: posts.map((post) => ({
+      link: `blog/${post.slug}`,
+      title: post.data.title,
+      pubDate: post.data.published,
+    })),
+  });
+};
