67ef6587e4c9df4770c01bbaa94f59b196a86102
penrodlol <christian@penrod.contact>
2024-03-14 21:40:24 -0400

    🏗️ Seed data for DB (post views)

--- db/config.ts
+++ db/config.ts
@@ -4,7 +4,7 @@ const PostView = defineTable({
   columns: {
     id: column.number({ primaryKey: true }),
     post: column.text({ unique: true }),
-    views: column.number({ default: 1 }),
+    views: column.number({ default: 0 }),
   },
 });
 
