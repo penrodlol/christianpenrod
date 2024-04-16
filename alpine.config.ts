584a7d54e1b17eeb324dfcd9a575c1807f6a0618
penrodlol <christian@penrod.contact>
2024-04-15 20:36:27 -0400

    🏗️ PostViews to alpinejs

--- alpine.config.ts
+++ alpine.config.ts
@@ -1,3 +1,8 @@
 import type { Alpine } from 'alpinejs';
 
-export default (Alpine: Alpine) => {};
+export default (Alpine: Alpine) => {
+  Alpine.magic('fetch', () => async (...props: Parameters<typeof fetch>) => {
+    if (props[1]?.body) props[1].body = JSON.stringify(props[1].body);
+    return await fetch(...props).then((res) => res.json());
+  });
+};
