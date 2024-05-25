aa96a3d92f5c0fac067e211ce151173eff608150
penrodlol <christian@penrod.contact>
2024-05-25 16:19:05 -0400

    ✨ Tabs

--- alpine.config.ts
+++ alpine.config.ts
@@ -1,9 +1,11 @@
 import type { Alpine } from 'alpinejs';
 // @ts-ignore\
 import collapse from '@alpinejs/collapse';
+// @ts-ignore
+import focus from '@alpinejs/focus';
 
 export default (Alpine: Alpine) => {
-  Alpine.plugin([collapse]);
+  Alpine.plugin([collapse, focus]);
 
   Alpine.magic('fetch', () => async (...props: Parameters<typeof fetch>) => {
     if (props[1]?.body) props[1].body = JSON.stringify(props[1].body);
---
+++ src/components/ui/tabs/index.ts
@@ -0,0 +1,5 @@
+// @ts-nocheck
+export { default as Root } from './components/root.astro';
+export { default as Tab } from './components/tab.astro';
+export { default as TabList } from './components/tablist.astro';
+export { default as TabPanel } from './components/tabpanel.astro';
