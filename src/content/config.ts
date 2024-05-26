b88e7936b82cce8ab91c1f1e8325c3150c280b4d
penrodlol <christian@penrod.contact>
2024-05-25 21:07:36 -0400

    ⚰️ Remove og images

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 
 const posts = defineCollection({
-  schema: ({ image }) =>
+  schema: () =>
     z.object({
       title: z.string(),
       description: z.string(),
       published: z.date(),
       topics: z.array(z.string()),
       repo: z.string().optional(),
-      cover: image(),
     }),
 });
 
