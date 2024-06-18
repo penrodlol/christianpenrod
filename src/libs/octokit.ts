a88a37f8b39887d085ea8645cb370a3ea9079af4
penrodlol <christian@penrod.contact>
2024-06-17 20:10:37 -0400

    ✨ Update astro env to remove need for "PUBLIC" prefix. Also support private usage within vercel serverless.

--- src/libs/octokit.ts
+++ src/libs/octokit.ts
@@ -1,7 +1,8 @@
 import { Octokit } from '@octokit/rest';
+import { GITHUB_TOKEN } from 'astro:env/server';
 
 const _global = global as typeof global & { octokit?: Octokit };
-const _octokit = _global.octokit || new Octokit({ auth: import.meta.env.GITHUB_TOKEN });
+const _octokit = _global.octokit || new Octokit({ auth: GITHUB_TOKEN });
 
 if (process.env.NODE_ENV !== 'development') _global.octokit = _octokit;
 
