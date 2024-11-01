8e953826380fec08625822946804bdba6baadf73
penrodlol <christian@penrod.contact>
2024-10-31 21:57:37 -0400

    🚧 Begin hero

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import db from '@astrojs/db';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel';
 import icon from 'astro-icon';
 import robotsTxt from 'astro-robots-txt';
 import { defineConfig, envField } from 'astro/config';
