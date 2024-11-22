28d7b9b622692d570f2481fa5af67565ec867c4e
penrodlol <christian@penrod.contact>
2024-11-21 19:47:42 -0500

    🔥 Remove some stuff temporarily

--- src/components/ui/tabs/index.ts
+++
@@ -1,5 +0,0 @@
-// @ts-nocheck
-export { default as Root } from './components/root.astro';
-export { default as Tab } from './components/tab.astro';
-export { default as TabList } from './components/tablist.astro';
-export { default as TabPanel } from './components/tabpanel.astro';
