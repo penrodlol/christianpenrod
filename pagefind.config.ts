5982b4eb202641c54ddb657e894764c2adff3da8
penrodlol <christian@penrod.contact>
2025-02-02 19:42:11 -0500

    🐛 Copy pagefind output to vercel static dir as well

--- pagefind.config.ts
+++ pagefind.config.ts
@@ -4,19 +4,21 @@ import { fileURLToPath } from 'node:url';
 import { createIndex } from 'pagefind';
 import sirv from 'sirv';
 
-const DIRECTORY = fileURLToPath(new URL('dist', import.meta.url));
+const DEFAULT_DIRECTORY = fileURLToPath(new URL('dist', import.meta.url));
+const VERCEL_DIRECTORY = fileURLToPath(new URL('.vercel/output/static', import.meta.url));
 
 export default (): AstroIntegration => ({
   name: 'pagefind',
   hooks: {
     'astro:server:setup': ({ server }) => {
-      const serve = sirv(DIRECTORY, { dev: true, etag: true });
+      const serve = sirv(DEFAULT_DIRECTORY, { dev: true, etag: true });
       server.middlewares.use((req, res, next) => (req.url?.includes('pagefind') ? serve(req, res, next) : next()));
     },
     'astro:build:done': async () => {
       const { index } = await createIndex();
-      await index?.addDirectory({ path: DIRECTORY });
-      await index?.writeFiles({ outputPath: join(DIRECTORY, 'pagefind') });
+      await index?.addDirectory({ path: DEFAULT_DIRECTORY });
+      await index?.writeFiles({ outputPath: join(DEFAULT_DIRECTORY, 'pagefind') });
+      await index?.writeFiles({ outputPath: join(VERCEL_DIRECTORY, 'pagefind') });
     },
   },
 });
