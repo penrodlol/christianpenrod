927419c888ccad49116cd086fc14b5fda5adfc4d
penrodlol <christian@penrod.contact>
2025-02-04 21:40:37 -0500

    🍱 OG image with new font and banners

--- src/pages/blog/[id].png.ts
+++ src/pages/blog/[id].png.ts
@@ -7,27 +7,31 @@ import { html } from 'satori-html';
 export const getStaticPaths: GetStaticPaths = async () =>
   await getSortedPosts().then((posts) => posts.map((post) => ({ params: { id: post.id }, props: post.data })));
 
-export const GET: APIRoute<GetSortedPosts[0]['data']> = async ({ props: post }) => {
-  const image = (await readFile('./src/assets/me.png')).toString('base64');
-  const fontNormal = await readFile('./node_modules/@fontsource/geist-sans/files/geist-sans-latin-400-normal.woff');
-  const fontBold = await readFile('./node_modules/@fontsource/geist-sans/files/geist-sans-latin-700-normal.woff');
+export const GET: APIRoute<GetSortedPosts[0]['data']> = async ({ props: post, params: { id } }) => {
+  const imageBanner = (await readFile(`./src/content/${id}.dark.png`)).toString('base64');
+  const imageMe = (await readFile('./src/assets/me.png')).toString('base64');
+  const fontSans = await readFile('./node_modules/@fontsource/geist-sans/files/geist-sans-latin-400-normal.woff');
+  const fontSerif = await readFile('./node_modules/@fontsource/eb-garamond/files/eb-garamond-latin-500-normal.woff');
 
   const template = html`
-    <div class="flex h-full w-full flex-col justify-center bg-black p-10">
-      <span class="text-8xl font-semibold text-neutral-50">${post.title}</span>
-      <div class="mt-auto flex items-center justify-between">
-        <div class="flex items-center text-4xl text-gray-300">
-          <span class="mr-8">${post.published.toLocaleDateString()}</span>
-          <span>${post.readingTime}</span>
-        </div>
-        <div class="flex items-center">
-          <div
-            class="mr-6 flex h-20 w-20 rounded-full border-4 border-neutral-600"
-            style="background-image: url(data:image/png;base64,${image}); background-size: 100% 100%;"
-          />
-          <div class="flex flex-col">
-            <span class="text-4xl font-semibold text-neutral-50">CHRISTIAN PENROD</span>
-            <span class="text-4xl text-gray-300">Front-End Web Developer</span>
+    <div class="relative flex h-full w-full flex-col justify-center bg-black p-10">
+      <div
+        class="absolute inset-10 flex h-full w-full opacity-50"
+        style="background-image: url(data:image/png;base64,${imageBanner}); background-size: 100% 100%;"
+      />
+      <div class="flex h-full w-full flex-col justify-between bg-black/10 p-10">
+        <span style="font-family:serif" class="text-8xl text-neutral-300">${post.title}</span>
+        <div style="font-family:sans" class="flex items-center justify-between text-neutral-400">
+          <div class="flex items-center text-4xl">
+            <span class="mr-8">${post.published.toLocaleDateString()}</span>
+            <span>${post.readingTime}</span>
+          </div>
+          <div class="flex items-center">
+            <div
+              class="mr-6 flex h-20 w-20 rounded-full border-4 border-neutral-600 bg-black"
+              style="background-image: url(data:image/png;base64,${imageMe}); background-size: 100% 100%;"
+            />
+            <span class="text-4xl font-semibold text-neutral-300">CHRISTIAN PENROD</span>
           </div>
         </div>
       </div>
@@ -37,8 +41,8 @@ export const GET: APIRoute<GetSortedPosts[0]['data']> = async ({ props: post }) 
   return satoriAstroOG({ template, width: 1200, height: 630 }).toResponse({
     satori: {
       fonts: [
-        { name: 'Geist Sans', data: fontNormal, style: 'normal', weight: 400 },
-        { name: 'Geist Sans', data: fontBold, style: 'normal', weight: 700 },
+        { name: 'sans', data: fontSans, style: 'normal', weight: 400 },
+        { name: 'serif', data: fontSerif, style: 'normal', weight: 500 },
       ],
     },
   });
--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -5,8 +5,7 @@ import { html } from 'satori-html';
 
 export const GET: APIRoute = async () => {
   const image = (await readFile('./src/assets/me.png')).toString('base64');
-  const fontNormal = await readFile('./node_modules/@fontsource/geist-sans/files/geist-sans-latin-400-normal.woff');
-  const fontBold = await readFile('./node_modules/@fontsource/geist-sans/files/geist-sans-latin-700-normal.woff');
+  const font = await readFile('./node_modules/@fontsource/eb-garamond/files/eb-garamond-latin-700-normal.woff');
 
   const template = html`
     <div class="flex h-full w-full flex-col items-center justify-center bg-black p-10">
@@ -14,17 +13,12 @@ export const GET: APIRoute = async () => {
         class="absolute -top-32 -left-96 z-0 flex h-[200%] w-240 opacity-40"
         style="background-image: url(data:image/png;base64,${image}); background-size: 100% 100%"
       />
-      <span class="text-8xl font-semibold text-neutral-50">CHRISTIAN PENROD</span>
-      <span class="text-7xl font-semibold text-neutral-300">Front-End Web Developer</span>
+      <span class="text-8xl text-neutral-50">CHRISTIAN PENROD</span>
+      <span class="text-7xl text-neutral-300">Front-End Web Developer</span>
     </div>
   `;
 
   return satoriAstroOG({ template, width: 1200, height: 630 }).toResponse({
-    satori: {
-      fonts: [
-        { name: 'Geist Sans', data: fontNormal, style: 'normal', weight: 400 },
-        { name: 'Geist Sans', data: fontBold, style: 'normal', weight: 700 },
-      ],
-    },
+    satori: { fonts: [{ name: 'font', data: font, style: 'normal', weight: 700 }] },
   });
 };
