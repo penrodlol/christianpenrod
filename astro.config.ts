0a2902560ed74c80e86016853322f01b63ef3f17
penrodlol <christian@penrod.contact>
2025-04-12 22:48:53 -0400

    ➕ astro=pagefind

--- astro.config.ts
+++ astro.config.ts
@@ -5,13 +5,13 @@ import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
+import pagefind from 'astro-pagefind';
 import robotsTxt from 'astro-robots-txt';
 import { defineConfig, envField } from 'astro/config';
 import { toString } from 'mdast-util-to-string';
 import readingTime from 'reading-time';
 import prettyCode from 'rehype-pretty-code';
 import { createCssVariablesTheme } from 'shiki';
-import pagefind from './pagefind.config';
 
 export default defineConfig({
   site: 'https://christianpenrod.com',
